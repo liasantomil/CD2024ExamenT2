109bebf033a4a2a6dd84a13ea7bc11a0afdfecb8
liasantomil <liasantomilrod@gmail.com>
2025-03-17 10:04:41 +0100

    rematado e3 + e4

---
+++ e3/src/main/java/liasantomil/ListaTareas.java
@@ -0,0 +1,27 @@
+package main.java.liasantomil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTareas {
+    private List<String> tareasPendientes = new ArrayList<>();
+
+    public void agregarTarea(String tarea) {
+        tareasPendientes.add(tarea);
+    }
+
+    public void marcarTareaComoCompletada(int indice) {
+        if (indice >= 0 && indice < tareasPendientes.size()) {
+
+            tareasPendientes.remove(indice);
+        } else {
+
+            throw new IndexOutOfBoundsException("Indice fuera del rango.");
+        }
+
+    }
+
+    public int obtenerNumeroTareasPendientes() {
+        return tareasPendientes.size();
+    }
+}
