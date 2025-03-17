cb35f6212a7b02b1e1ba4bf239d8d1d326748308
liasantomil <liasantomilrod@gmail.com>
2025-03-17 10:14:43 +0100

    añadidos comentarios

--- e2/src/test/java/com/liasantomil/ArrayUtilsTest.java
+++ e2/src/test/java/com/liasantomil/ArrayUtilsTest.java
@@ -4,36 +4,43 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class ArrayUtilsTest {
+
+    /*encontrar mínimo*/
     @Test    
     void testEncontrarMinimo() {
         int[] array = {3, 1, 4, 1, 5, 9, 2};
         assertEquals(1, ArrayUtils.encontrarMinimo(array));
     }
 
+    /*minimo arrayVacio*/
     @Test
     void testEncontrarMinimoArrayVacio() {
         int[] arrayVacio = {};
         assertThrows(IllegalArgumentException.class, () -> ArrayUtils.encontrarMinimo(arrayVacio));
     }
 
+    /*encontrar máximo */
     @Test 
     void testEncontrarMaximo() {
         int[] array = {3, 1, 4, 1, 5, 9, 2};
         assertEquals(9, ArrayUtils.encontrarMaximo(array));
     }
 
+    /*máximo arrayVacio */
     @Test 
     void testEncontrarMaximoArrayVacio(){
         int[] arrayVacio = {};
         assertThrows(IllegalArgumentException.class, () -> ArrayUtils.encontrarMaximo(arrayVacio));
     }
 
+    /*calcular promedio*/
     @Test
     void testCalcularPromedio() {
         int[] array = {3, 1, 4, 1, 5, 9, 2};
         assertEquals(3.57, ArrayUtils.calcularPromedio(array));
     }
 
+    /*promedio arrayVacio */
     @Test 
     void testCalcularPromedioArrayVacio(){
         int[] arrayVacio = {};
--- e3/src/main/java/com/liasantomil/ListaTareas.java
+++ e3/src/main/java/com/liasantomil/ListaTareas.java
@@ -6,21 +6,21 @@ import java.util.List;
 public class ListaTareas {
     private List<String> tareasPendientes = new ArrayList<>();
 
+    /*tareas pendientes */
     public void agregarTarea(String tarea) {
         tareasPendientes.add(tarea);
     }
 
+    /*tareas completadas */
     public void marcarTareaComoCompletada(int indice) {
         if (indice >= 0 && indice < tareasPendientes.size()) {
-
             tareasPendientes.remove(indice);
         } else {
-
             throw new IndexOutOfBoundsException("Indice fuera del rango.");
         }
-
     }
 
+    /*tamaño tareas pendientes */
     public int obtenerNumeroTareasPendientes() {
         return tareasPendientes.size();
     }
